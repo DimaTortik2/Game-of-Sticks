857066a3a736f6d23c4d1bc8f268301a9dea0d67
DimaTortik2 <dimtar009@gmail.com>
2025-07-26 17:13:32 +0300

    add "do sticks" to main-menu-page

--- src/shared/ui/bg/main-page-background.tsx
+++ src/shared/ui/bg/main-page-background.tsx
@@ -8,12 +8,38 @@ export const MainPageBackground = () => {
         '
 			/>
 
-			<div
+			{/* <div
 				className='
           absolute bg-[#d9d9d9] w-[30%] h-[300%] transform
           -rotate-25 top-[-70%] left-[20%]
         '
-			/>
+			>
+				<div
+					className=' absolute left-[58%] top-[40%] transform
+          rotate-89 bg-red-400 w-[500px]'
+				>
+					<p className='font-semibold text-5xl text-[#FFD700]'>DO STICKS!</p>
+				</div>
+				
+			</div> */}
+
+			<div
+				className='
+          absolute bg-[#d9d9d9] w-[400px] h-[280%]  flex items-center 
+					transform
+          -rotate-25
+					left-[300px]
+        '
+			>
+				<div
+					className='transform rotate-90
+            w-[400px] mx-[64%] bg-[#212121] p-2 rounded-2xl'
+				>
+					<p className='font-semibold text-7xl w-[50rem] text-[#e1cb4f]'>
+						DO STICKS!
+					</p>
+				</div>
+			</div>
 		</>
 	)
 }
